9f05345ed5d2c510d4055eb614df88acaa1ac3d0
AlexBl21 <hblancorolon@gmail.com>
2024-08-26 11:38:46 -0500

    formulario de registro

--- scripts.js
+++ scripts.js
@@ -1,15 +1,29 @@
+
 document.addEventListener("DOMContentLoaded",function() {
     var loginForm= document.getElementById("loginForm");
+    if (loginForm){
     loginForm.addEventListener("submit", function(event) {
         event.preventDefault();
-        var username = document.getElementById("username").value;
+        var username = document.getElementById("userName").value;
         var password = document.getElementById("password").value;
 
-        if (username==="admin" && password==="1234"){
+        if (username === "admin" && password === "1234"){
             window.location.href="dashboard.html";
         }
         else{
             alert("Invalid username or password");
         }
     });
+    }
+    var registroForm= document.getElementById("registroForm");
+    if (registroForm){
+        registroForm.addEventListener("submit", function(event) {
+            event.preventDefault();
+            var newUser=document.getElementById("newUser").value;
+            var newPass=document.getElementById("newPassword").value;
+            alert("Usuario registrado: "+ newuser);
+            window.location.href= "index.html";
+        });
+    }
 });
+
